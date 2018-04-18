c454c750f0193e13ca308e144a21ce2a953d3595
Tyson Au <au.tyson@gmail.com>
2018-04-17 21:24:24 -0600

    Add conditional rendering for the query results

--- client/src/components/Query.js
+++ client/src/components/Query.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import { TextField, Typography } from 'material-ui';
-import Card, {
-  CardActions,
-  CardContent,
-} from 'material-ui/Card';
+import List, {
+  ListItem,
+  ListItemText,
+} from 'material-ui/List';
 
 class Query extends React.Component {
-  state = { cards: [], results: [], query: '' };
+  state = {
+    cards: [],
+    results: [],
+    query: '',
+    toggleQuery: false,
+  };
 
   componentDidMount() {
     axios.get('/api/query/').then((res) => {
@@ -23,53 +28,63 @@ class Query extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.queryCards();
-    this.setState({ query: '' });
+    this.setState({ query: '', toggleQuery: true });
   };
 
   queryCards = () => {
     const { cards, query } = this.state;
     let results = [];
     cards.map((card) => {
-      card.name
-        .toLowerCase()
-        .includes(query.toLowerCase())
+      card.name.toLowerCase().includes(query.toLowerCase())
         ? results.push(card)
         : null;
     });
     this.setState({ results: results });
   };
 
-  displayResults = () => {
-    const { results } = this.state;
-    return results.map((result, index) => {
-      return (
-        <Card key={index + 1}>
-          {/* <CardMedia /> */}
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="headline"
-              component="h2">
-              {result.name}
-            </Typography>
-            <Typography component="p">
-              {result.flavor}
-            </Typography>
-          </CardContent>
-          <CardActions />
-        </Card>
-      );
+  sortCards = (cards) => {
+    cards.sort((a, b) => {
+      return a.cost - b.cost;
     });
   };
 
+  displayNotFound = () => {
+    return (
+      <Typography
+        color="blue"
+        variant="button"
+        align="center">
+        V-07-TR-0N C0U7D N0T FIND THE CARD Y0U WERE 700KING
+        F0R. TRY AGAIN.
+      </Typography>
+    );
+  };
+  displayResults = () => {
+    const { results } = this.state;
+    return (
+      <List>
+        {results.map((result, index) => {
+          return (
+            <ListItem>
+              <ListItemText
+                primary={`[${result.cost}] ${result.name}`}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+
   render() {
+    const { results, toggleQuery } = this.state;
     return (
       <div style={container}>
         <form onSubmit={this.handleSubmit}>
           <TextField
             style={searchBar}
-            autoFocus
             fullWidth
+            autoComplete="off"
             name="query"
             placeholder="(e.g. Alexstrasza, Grim Patron, Kazakus, and etc.)"
             label="Search through the Innkeeper's Hearthstone collection..."
@@ -77,7 +92,11 @@ class Query extends React.Component {
             onChange={this.handleChange}
           />
         </form>
-        {this.displayResults()}
+        <div>
+          {results.length === 0 && toggleQuery === true
+            ? this.displayNotFound()
+            : this.displayResults()}
+        </div>
       </div>
     );
   }
