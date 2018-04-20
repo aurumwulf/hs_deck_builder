cf829b25e75633fa4aef89cca4b8913509feca2e
Tyson Au <au.tyson@gmail.com>
2018-04-20 13:56:15 -0600

    Delete decklist json variable from schema

--- db/migrate/20180419164838_create_decks.rb
+++ db/migrate/20180419164838_create_decks.rb
@@ -2,7 +2,6 @@ class CreateDecks < ActiveRecord::Migration[5.1]
   def change
     create_table :decks do |t|
       t.string :name
-      t.json :decklist
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20180419164838) do
 
   create_table "decks", force: :cascade do |t|
     t.string "name"
-    t.json "decklist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
